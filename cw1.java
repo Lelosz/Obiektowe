15871e12ffa7a65bd7844c085969c129f83cd8c8
Lelosz <73462445+Lelosz@users.noreply.github.com>
2021-10-15 00:51:43 +0200

    cw1

---
+++ cw1.java
@@ -0,0 +1,89 @@
+public class cw1 {
+    public static void main(String[] args){
+        System.out.println("Zadanie 1");
+        System.out.println(31+31+29);
+        System.out.println("\n");
+        System.out.println("Zadanie 2");
+        int suma = 0;
+        int i = 1;
+        while(i<11){
+            suma = suma + i;
+            i++;
+        }
+        System.out.println(suma);
+        System.out.println("\n");
+        System.out.println("Zadanie 3");
+        suma = 1;
+        i = 1;
+        while(i<11){
+            suma = suma * i;
+            i++;
+        }
+        System.out.println(suma);
+        System.out.println("\n");
+        System.out.println("Zadanie 4");
+        System.out.println(1000*1.06);
+        System.out.println(1000*1.06*1.06);
+        System.out.println(1000*1.06*1.06*1.06);
+        System.out.println("\n");
+        System.out.println("Zadanie 5\n");
+        System.out.println("""
+                +----+
+                |java|
+                +----+
+                """);
+        System.out.println("Zadanie 6\n");
+        System.out.println("""
+                 /////
+                +'''''+
+                (|ʘ ʘ|)
+                  |ˠ|
+                 | Ѡ |
+                 +---+
+                """);
+        System.out.println("Zadanie 10\n");
+        System.out.println("""
+                1. Andrzej Suski
+                2. Tadeusz Rydzyk
+                3. Mateusz Morawiecki
+                """);
+        System.out.println("Zadanie 11\n");
+        System.out.println("""
+                Co innego cebula.
+                Ona nie ma wnętrzności.
+                Jest sobą na wskroś cebula
+                do stopnia cebuliczności.
+                Cebulasta na zewnątrz,
+                cebulowa do rdzenia,
+                mogłaby wejrzeć w siebie
+                cebula bez przerażenia.
+                                
+                W nas obczyzna i dzikość
+                ledwie skórą przykryta,
+                inferno w nas interny,
+                anatomia gwałtowna,
+                a w cebuli cebula,
+                nie pokrętne jelita.
+                Ona wielekroć naga,
+                do głębi itympodobna.
+                                
+                Byt niesprzeczny cebula,
+                udany cebula twór.
+                W jednej po prostu druga,
+                w większej mniejsza zawarta,
+                a w następnej kolejna,
+                czyli trzecia i czwarta.
+                Dośrodkowa fuga.
+                Echo złożone w chór.
+                                
+                Cebula, to ja rozumiem:
+                najnadobniejszy brzuch świata.
+                Sam się aureolami
+                na własną chwałę oplata.
+                W nas – tłuszcze, nerwy, żyły,
+                śluzy i sekretności.
+                I jest nam odmówiony
+                idiotyzm doskonałości.
+                """);
+    }
+}
